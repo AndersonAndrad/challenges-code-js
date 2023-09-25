2e3781d94c1ac89ec7c880973679a9dd8339979c
Anderson Andrade <anderson_andrade_@outlook.com>
2023-09-25 01:16:43 -0300

    refactor: simplify code to check of number is divisible by 3 or 5

--- codes/multiple3Or5.challenge.js
+++ codes/multiple3Or5.challenge.js
@@ -15,18 +15,9 @@ const solution = (number) => {
    let finalNumber = 0;
 
    for(let index = 0; index < number; index++){
-       let intercept = index / 3;
+       let divisible = index % 3 === 0 || index % 5 === 0;
 
-       if(intercept % 1 === 0) {
-           finalNumber = finalNumber + index;
-           continue
-       }
-
-       intercept = index / 5;
-
-       if(intercept % 1 === 0){
-           finalNumber = finalNumber + index;
-       }
+       if(divisible) finalNumber = finalNumber + index;
    }
 
    if(finalNumber < 0) return 0;
